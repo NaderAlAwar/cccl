ed3f1d574587553a95306a30395948bbef991ba5
Nader Al Awar <naderalawar@gmail.com>
2025-06-26 21:34:21 +0000

    Go back to initial tuning for test on T4 histogram

--- c/parallel/src/histogram.cu
+++ c/parallel/src/histogram.cu
@@ -93,12 +93,13 @@ struct histogram_kernel_source
 histogram_runtime_tuning_policy
 get_policy(int /*cc*/, [[maybe_unused]] cccl_type_info sample_t, [[maybe_unused]] int num_active_channels)
 {
-  // const int v_scale                      = (sample_t.size + sizeof(int) - 1) / sizeof(int);
-  // constexpr int nominal_items_per_thread = 16;
+  const int v_scale                      = (sample_t.size + sizeof(int) - 1) / sizeof(int);
+  constexpr int nominal_items_per_thread = 16;
 
-  // int pixels_per_thread = (::cuda::std::max) (nominal_items_per_thread / num_active_channels / v_scale, 1);
+  int pixels_per_thread = (::cuda::std::max)(nominal_items_per_thread / num_active_channels / v_scale, 1);
 
-  return {960, 12};
+  return {384, pixels_per_thread};
+  // return {960, 12};
 }
 
 std::string get_init_kernel_name(int num_active_channels, std::string_view counter_t, std::string_view offset_t)
@@ -237,11 +238,11 @@ struct __align__({1}) storage_t {
 struct agent_policy_t {
   static constexpr int BLOCK_THREADS = {3};
   static constexpr int PIXELS_PER_THREAD = {4};
-  static constexpr bool IS_RLE_COMPRESS = false;
+  static constexpr bool IS_RLE_COMPRESS = true;
   static constexpr cub::BlockHistogramMemoryPreference MEM_PREFERENCE = cub::SMEM;
   static constexpr bool IS_WORK_STEALING = false;
-  static constexpr int VEC_SIZE = 1 << 2;
-  static constexpr cub::BlockLoadAlgorithm LOAD_ALGORITHM = cub::BLOCK_LOAD_WARP_TRANSPOSE;
+  static constexpr int VEC_SIZE = 4;
+  static constexpr cub::BlockLoadAlgorithm LOAD_ALGORITHM = cub::BLOCK_LOAD_DIRECT;
   static constexpr cub::CacheLoadModifier LOAD_MODIFIER = cub::LOAD_DEFAULT;
 };
 struct {5} {
