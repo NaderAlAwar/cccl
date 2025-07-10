27242f6b71bb926a67f1f4cae26006eb9c7ef5fb
anon <users.noreply.github.com>
2025-07-10 23:45:45 +0000

    add unique by key benchmarks

---
+++ python/cuda_cccl/benchmarks/parallel/overheads/bench_unique_by_key_low_overhead_no_nvbench.py
@@ -0,0 +1,74 @@
+import time
+
+import numpy as np
+import torch
+
+import cuda.cccl.parallel.experimental.algorithms as algorithms
+
+
+def unique_by_key():
+    n_elems = 2**20
+
+    d_in_keys = torch.rand(n_elems, dtype=torch.float32, device="cuda")
+    d_in_items = torch.rand(n_elems, dtype=torch.float32, device="cuda")
+    d_out_keys = torch.rand(n_elems, dtype=torch.float32, device="cuda")
+    d_out_items = torch.rand(n_elems, dtype=torch.float32, device="cuda")
+    d_out_num_selected = torch.empty(1, dtype=torch.int32, device="cuda")
+
+    def compare_op(lhs, rhs):
+        return np.uint8(lhs == rhs)
+
+    alg = algorithms.unique_by_key_low_overhead(
+        d_in_keys, d_in_items, d_out_keys, d_out_items, d_out_num_selected, compare_op
+    )
+
+    # query size of temporary storage and allocate
+    temp_nbytes = alg(
+        None,
+        0,
+        d_in_keys.data_ptr(),
+        d_in_items.data_ptr(),
+        d_out_keys.data_ptr(),
+        d_out_items.data_ptr(),
+        d_out_num_selected.data_ptr(),
+        n_elems,
+    )
+
+    temp_storage = torch.empty(temp_nbytes, dtype=torch.uint8, device="cuda")
+
+    for _ in range(10):
+        alg(
+            temp_storage.data_ptr(),
+            temp_storage.nbytes,
+            d_in_keys.data_ptr(),
+            d_in_items.data_ptr(),
+            d_out_keys.data_ptr(),
+            d_out_items.data_ptr(),
+            d_out_num_selected.data_ptr(),
+            n_elems,
+        )
+
+    torch.cuda.synchronize()
+
+    execution_times = []
+    for _ in range(30):
+        start = time.perf_counter_ns()
+        alg(
+            temp_storage.data_ptr(),
+            temp_storage.nbytes,
+            d_in_keys.data_ptr(),
+            d_in_items.data_ptr(),
+            d_out_keys.data_ptr(),
+            d_out_items.data_ptr(),
+            d_out_num_selected.data_ptr(),
+            n_elems,
+        )
+        stop = time.perf_counter_ns()
+        execution_times.append(stop - start)
+
+    avg_time_ns = sum(execution_times) / len(execution_times)
+    print(f"Num elems {n_elems}; Average execution time: {avg_time_ns:.2f} ns")
+
+
+if __name__ == "__main__":
+    unique_by_key()
